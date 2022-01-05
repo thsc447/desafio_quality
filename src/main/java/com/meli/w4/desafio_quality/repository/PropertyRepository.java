c3667ab267fd4c8b12798dc37a9f69cb8a5dc156
thsc447 <thiago.campos@mercadolivre.com>
2022-01-05 12:56:57 -0300

    test: add totalArea feature

--- src/main/java/com/meli/w4/desafio_quality/repository/PropertyRepository.java
+++ src/main/java/com/meli/w4/desafio_quality/repository/PropertyRepository.java
@@ -1,7 +1,15 @@
 package com.meli.w4.desafio_quality.repository;
 
+import com.meli.w4.desafio_quality.entity.Property;
+import com.meli.w4.desafio_quality.entity.Room;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class PropertyRepository {
+
+    public List<Room> save(List<?> property){
+        return null;
+    }
 }
--- src/test/java/com/meli/w4/desafio_quality/service/PropertyServiceTest.java
+++ src/test/java/com/meli/w4/desafio_quality/service/PropertyServiceTest.java
@@ -1,5 +1,48 @@
 package com.meli.w4.desafio_quality.service;
 
+import com.meli.w4.desafio_quality.entity.Property;
+import com.meli.w4.desafio_quality.entity.Room;
+import com.meli.w4.desafio_quality.repository.PropertyRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PropertyServiceTest {
+
+    @Test
+    public void shouldReturnTheTotalArea(){
+        List<Room> rooms = ListOfRooms();
+        Double result = makeSUT().calculateTotalArea(rooms);
+        assertEquals(2,result);
+    }
+
+    public PropertyService makeSUT(){
+        List<Room> rooms = ListOfRooms();
+        PropertyRepository mock = Mockito.mock(PropertyRepository.class);
+        Mockito.when(mock.save(rooms)).thenReturn(rooms);
+        PropertyService propertyService = new PropertyService(mock);
+        return propertyService;
+    }
+
+    public List<Room> ListOfRooms(){
+        List<Room> rooms = Arrays.asList(
+                Room.builder()
+                        .room_name("1")
+                        .room_width(1d)
+                        .room_length(1d)
+                        .build(),
+                Room.builder()
+                        .room_name("2")
+                        .room_width(1d)
+                        .room_length(1d)
+                        .build()
+        );
+        return rooms;
+    }
 
 }
