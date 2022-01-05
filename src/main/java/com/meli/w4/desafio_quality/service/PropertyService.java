f68fc717dd25cdca0f536d53e0afd558227ff511
Francosta08 <francisco.fcosta@mercadolivre.com>
2022-01-05 12:19:45 -0300

    Criado metodo calcular area Room

--- src/main/java/com/meli/w4/desafio_quality/service/PropertyService.java
+++ src/main/java/com/meli/w4/desafio_quality/service/PropertyService.java
@@ -1,9 +1,13 @@
 package com.meli.w4.desafio_quality.service;
 
 import com.meli.w4.desafio_quality.entity.Property;
+import com.meli.w4.desafio_quality.entity.Room;
 import com.meli.w4.desafio_quality.repository.PropertyRepository;
 import com.meli.w4.desafio_quality.response.PropertyResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PropertyService {
@@ -16,5 +20,13 @@ public class PropertyService {
 
     public PropertyResponse calculateArea(Property property) {
         return new PropertyResponse();
+    }
+
+    public double getPropetyArea(List<Room> rooms){
+        double totalArea = 0;
+        for (Room room: rooms) {
+            totalArea += room.getRoom_length() * room.getRoom_width();
+        }
+        return totalArea;
     }
 }
