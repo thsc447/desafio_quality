e7b77fbaf771c755715b4babd56ae3c4a3aa8a74
thsc447 <thiago.campos@mercadolivre.com>
2022-01-05 12:07:23 -0300

    feat: add method  calculateTotalArea

--- src/main/java/com/meli/w4/desafio_quality/service/PropertyService.java
+++ src/main/java/com/meli/w4/desafio_quality/service/PropertyService.java
@@ -1,11 +1,14 @@
 package com.meli.w4.desafio_quality.service;
 
 import com.meli.w4.desafio_quality.entity.Property;
+import com.meli.w4.desafio_quality.entity.Room;
 import com.meli.w4.desafio_quality.repository.PropertyRepository;
 import com.meli.w4.desafio_quality.response.PropertyResponse;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PropertyService {
@@ -20,7 +23,15 @@ public class PropertyService {
         return new PropertyResponse();
     }
 
-    public BigDecimal calculateTotalPriceOfProperty(Property property){
+
+    public Double calculateTotalArea(List<Room> rooms) {
+        return rooms.stream()
+                .map(room -> room.getRoom_length() * room.getRoom_width())
+                .reduce((a, b) -> a + b)
+                .get();
+    }
+
+    public BigDecimal calculateTotalPriceOfProperty(Property property) {
         return null;
     }
 }
