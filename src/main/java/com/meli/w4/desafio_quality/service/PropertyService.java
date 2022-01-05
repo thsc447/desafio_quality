929f27951a8157536e40ae565a6257158ec25575
thsc447 <thiago.campos@mercadolivre.com>
2022-01-05 11:42:40 -0300

    chore: add calculateTotalPriceOfProperty method

--- src/main/java/com/meli/w4/desafio_quality/service/PropertyService.java
+++ src/main/java/com/meli/w4/desafio_quality/service/PropertyService.java
@@ -5,6 +5,8 @@ import com.meli.w4.desafio_quality.repository.PropertyRepository;
 import com.meli.w4.desafio_quality.response.PropertyResponse;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 public class PropertyService {
 
@@ -16,5 +18,9 @@ public class PropertyService {
 
     public PropertyResponse calculateArea(Property property) {
         return new PropertyResponse();
+    }
+
+    public BigDecimal calculateTotalPriceOfProperty(Property property){
+        return null;
     }
 }
