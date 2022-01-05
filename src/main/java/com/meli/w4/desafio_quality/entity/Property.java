8c96d1477d5fd661c4bf232d9cb7fb95842324b0
Francosta08 <francisco.fcosta@mercadolivre.com>
2022-01-05 14:39:43 -0300

    Alteração

--- src/main/java/com/meli/w4/desafio_quality/entity/Property.java
+++ src/main/java/com/meli/w4/desafio_quality/entity/Property.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -14,6 +17,8 @@ import java.util.List;
 @AllArgsConstructor
 public class Property {
     private String prop_name;
+    @NotBlank(message = "O Nome da propriedade não pode estar vazio.")
+    @Length(min = 1 , max = 30 , message = "O nome da propriedade deve começar com uma letra maiúscula.")
     private String prop_district;
     private BigDecimal value_district_m2;
     private List<Room> rooms;
